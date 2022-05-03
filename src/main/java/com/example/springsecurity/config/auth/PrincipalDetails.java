629bb9c9f2573377bbaf10e693b044cceeb30cbb
jaryeonge <jaryeonge@gmail.com>
2022-05-03 18:51:45 +0900

    Add endpoint to User and OAuthUser

--- src/main/java/com/example/springsecurity/config/auth/PrincipalDetails.java
+++ src/main/java/com/example/springsecurity/config/auth/PrincipalDetails.java
@@ -1,11 +1,14 @@
 package com.example.springsecurity.config.auth;
 
 import com.example.springsecurity.model.User;
+import lombok.Data;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Map;
 
 // 시큐리티가 /login 주소 요청이 오면 낚아채서 로그인을 진행시킨다.
 // 로그인 진행이 완료가 되면 시큐리티 session 을 만들어준다. (Security Contextholder)
@@ -15,12 +18,18 @@ import java.util.Collection;
 
 // Security Session => Authentication => UserDetails
 
-public class PrincipalDetails implements UserDetails {
+@Data
+public class PrincipalDetails implements UserDetails, OAuth2User {
 
     private User user;
 
     public PrincipalDetails(User user) {
         this.user = user;
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return null;
     }
 
     @Override
@@ -67,5 +76,10 @@ public class PrincipalDetails implements UserDetails {
         // 현재시간 - 로그인시간 => 1년 초과? return false
 
         return true;
+    }
+
+    @Override
+    public String getName() {
+        return null;
     }
 }
